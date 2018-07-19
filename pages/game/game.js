d9bc3ef2478b6369c1871b261d8e172f0c55ed55
chuzhumin98 <chuzhumin98@gmail.com>
2018-07-19 11:07:56 +0800

    catch for the touch event

--- pages/game/game.js
+++ pages/game/game.js
@@ -12,6 +12,10 @@ for (let i = 0; i < grid_per_edge; i++) {
   }
 }
 
+let is_ontouch = false; //is on touch mobile or not
+let touch_startX = 0; //touch start position X coordinate
+let touch_startY = 0; //touch start position Y coordinate 
+
 function intDiv(num, den) {
   return Math.floor(num/den); //simplify as always positive condition
 }
@@ -40,5 +44,51 @@ Page({
   //get the states array's item string
   getStatesItemString: function (idx, idy) {
     return 'states['+idx+']['+idy+']';
+  },
+
+  /*
+  get the player's slide direction
+  none, left, right, up, dowm
+  */
+  getSlideDirection: function (startX, startY, endX, endY) {
+    let deltaX = endX - startX;
+    let deltaY = endY - startY;
+    if (Math.abs(deltaX) + Math.abs(deltaY) < 10) { //ignore too small slide
+      return 'none';
+    } else {
+      if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        if (deltaX > 0) {
+          return 'right';
+        } else {
+          return 'left';
+        }
+      } else {
+        if (deltaY > 0) {
+          return 'down';
+        } else {
+          return 'up';
+        }
+      }
+    }
+  },
+
+  onTouchStart: function (event) {
+    touch_startX = event.touches[0].clientX; //record the start touch position
+    touch_startY = event.touches[0].clientY;
+    is_ontouch = true; //change ontouch attribute to true
+    //console.log('start at '+event.touches[0].clientX+','+event.touches[0].clientY);
+  },
+
+  onTouchEnd: function (event) {
+    if (is_ontouch) {
+      is_ontouch = false; //set to prepare next slide process
+      let direction = this.getSlideDirection(touch_startX, touch_startY, event.changedTouches[0].clientX, event.changedTouches[0].clientY);
+      console.log(direction);
+    }
+    //console.log('end at '+event.changedTouches[0].clientX + ',' + event.changedTouches[0].clientY);
+  },
+
+  onTouchCancel: function (event) {
+    is_ontouch = false; //drop this touch process
   }
 })
